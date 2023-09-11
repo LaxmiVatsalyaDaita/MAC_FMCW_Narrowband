96ba566bd8370b98ec1fad1ce6c10acf8c213a75
LaxmiVatsalyaDaita <87368740+LaxmiVatsalyaDaita@users.noreply.github.com>
2023-09-11 11:25:14 +0530

    Add files via upload

---
+++ Simulations/different_slope.m
@@ -0,0 +1,40 @@
+% Define parameters
+fs = 1000;          % Sampling frequency (Hz)
+t = 0:1/fs:1;       % Time vector (0 to 1 second)
+freq1 = 6;          % Frequency of the first ramp waveform (Hz)
+freq2 = 5;          % Frequency of the second ramp waveform (Hz)
+delay = 0.01;        % Time delay between the two waveforms (seconds)
+
+% Generate the first ramp waveform
+waveform1 = sawtooth(2*pi*freq1*t) + 1;
+
+% Generate the second ramp waveform with time delay
+waveform2 = sawtooth(2*pi*freq2*(t)) + 1;
+
+% Calculate the instantaneous frequency of both waveforms
+%instantaneous_freq1 = diff(unwrap(angle(hilbert(waveform1)))) / (2*pi);
+%instantaneous_freq2 = diff(unwrap(angle(hilbert(waveform2)))) / (2*pi);
+
+frequency_difference = waveform1 - waveform2;
+
+% Calculate the frequency difference
+%frequency_difference = instantaneous_freq2 - instantaneous_freq1;
+
+% Plot the results
+subplot(3,1,1);
+plot(t, waveform1);
+title('Waveform 1');
+xlabel('Time (s)');
+ylabel('Frequency');
+
+subplot(3,1,2);
+plot(t, waveform2);
+title('Waveform 2 (Time-Delayed)');
+xlabel('Time (s)');
+ylabel('Frequency');
+
+subplot(3,1,3);
+plot(t, frequency_difference);
+title('Frequency Difference');
+xlabel('Time (s)');
+ylabel('Frequency Difference (Hz)');
